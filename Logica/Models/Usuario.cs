d15e15f9df4e870632ee4a22dd8470ad427e24aa
jeste <jeste@LAPTOP-HMNTIQK1>
2023-03-17 22:26:35 -0600

    Semana 9

--- Logica/Models/Usuario.cs
+++ Logica/Models/Usuario.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Logica.Services;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Logica.Models
 {
@@ -23,7 +25,7 @@ namespace Logica.Models
         public bool Activo { get; set; }
 
         //propiedades compuestas
-        Usuario_Rol MiRolTipo { get; set; }
+        public Usuario_Rol MiRolTipo { get; set; }
 
         //normalmente cuando tenemos propiedades compuestas con tipos que
         //hemos programado nosotros mismos, debemos instanciar dichas propiedades
@@ -70,6 +72,47 @@ namespace Logica.Models
 
             return R;
         }
+
+        public Usuario ConsultarPorIDRetornaUsuario()
+        {
+            Usuario R =  new Usuario();
+
+            Conexion MiCnn = new Conexion();
+
+            MiCnn.ListaDeParametros.Add(new SqlParameter("@ID", this.UsuarioID));
+
+            DataTable dt = new DataTable();
+
+
+            dt = MiCnn.EjecutarSELECT("SPUsuarioConsultarPorID");
+
+            if(dt != null && dt.Rows.Count>0)
+            {
+                //esta consulta deberia tener solo un registro
+                //se crea un objeto de tipo datarow para capturar la info
+                //contenida en index 0 del dt (datatable)
+                DataRow dr = dt.Rows[0];
+
+                R.UsuarioID = Convert.ToInt32(dr["UsuarioID"]);
+                R.UsuarioNombre = Convert.ToString(dr["UsuarioNombre"]);
+
+
+                R.UsuarioCedula = Convert.ToString(dr["UsuarioCedula"]);
+                R.UsuarioCorreo = Convert.ToString(dr["UsuarioCorreo"]);
+                R.UsuarioTelefono = Convert.ToString(dr["UsuarioTelefono"]);
+                R.UsuarioDireccion = Convert.ToString(dr["UsuarioDireccion"]);
+
+                R.UsuarioContrasennia = string.Empty;
+
+                //composiciones 
+                R.MiRolTipo.UsuarioRolID = Convert.ToInt32(dr["UsuarioRolID"]);
+                R.MiRolTipo.UsuarioRolDescripcion = Convert.ToString(dr["UsuarioRolDescripcion"]);
+
+            }
+
+            return R;
+        }
+
         public bool ConsultarPorCedula()
         {
             bool R = false;
@@ -86,6 +129,16 @@ namespace Logica.Models
         public DataTable ListarActivos()
         {
             DataTable R = new DataTable();
+
+            Conexion MiCnn = new Conexion();
+
+            //en este caso como el SP tiene un parametro, debemos por lo tanto
+            //definir ese parametro en la lista de parametros del objeto de conexion
+
+            MiCnn.ListaDeParametros.Add(new SqlParameter("@VerActivos", true));
+            R = MiCnn.EjecutarSELECT("SPUsuariosListar");
+
+
 
             return R;
         }
--- P52023_EstebanJ/Formularios/FrmUsuariosGestion.cs
+++ P52023_EstebanJ/Formularios/FrmUsuariosGestion.cs
@@ -12,9 +12,27 @@ namespace P52023_EstebanJ.Formularios
 {
     public partial class FrmUsuariosGestion : Form
     {
+
+        //por orden es mejor crear objetos locales que permitan 
+        //la gestion del tema que estamos tratando
+        //usando objetos individuales en la funcion puede provocar desorden y 
+        //complicar mas la lectura del codigo fuente
+
+        //objeto local para usuario
+
+        private Logica.Models.Usuario MiUsuarioLocal { get; set; }
+
+        //lista local de usuarios que se visualizan en el datagridview
+
+        private DataTable ListaUsuarios { get; set; }
+
+
         public FrmUsuariosGestion()
         {
             InitializeComponent();
+
+            MiUsuarioLocal = new Logica.Models.Usuario();
+            ListaUsuarios = new DataTable();
         }
 
         private void FrmUsuariosGestion_Load(object sender, EventArgs e)
@@ -24,9 +42,33 @@ namespace P52023_EstebanJ.Formularios
 
             CargarListaRoles();
 
+            CargarListaDeUsuarios();
+
 
 
         }
+
+        private void CargarListaDeUsuarios()
+        {
+            //resetear la lista de usuarios haciendo re instancia del objeto
+
+            ListaUsuarios = new DataTable();
+
+            if(CboxVerActivos.Checked)
+            {
+                ListaUsuarios = MiUsuarioLocal.ListarActivos();
+            }
+            else
+            {
+                ListaUsuarios = MiUsuarioLocal.ListarInactivos();
+
+            }
+
+            DgLista.DataSource = ListaUsuarios;
+            
+        }
+
+
         private void CargarListaRoles()
         {
             Logica.Models.Usuario_Rol MiRol = new Logica.Models.Usuario_Rol();
@@ -45,24 +87,72 @@ namespace P52023_EstebanJ.Formularios
 
         }
 
+        private void DgLista_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            DgLista.ClearSelection();
+
+        }
+
+        private void DgLista_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //cuando seleccionemos
+            //una fila del datagrid se debe cargar la info de dicho usuario
+            // en el usuario local y luego dibujar esa info en los controles graficos
+
+            if(DgLista.SelectedRows.Count ==1)
+            {
+                //TODO: Limpiar el formulario
+
+                //de la coleccion de filas seleccionadas(que en este caso es solo una)
+
+                DataGridViewRow MiFila = DgLista.SelectedRows[0];
+
+                //lo que necesito es el valor del ID del usuario para realizar la consulta
+                //y traer todos los datos para llenar el objeto de usuario local
+
+                int IdUsuario = Convert.ToInt32(MiFila.Cells["CUsuarioID"].Value);
+
+                //para no asumir riesgos se reinstancia el usuario local
+                MiUsuarioLocal = new Logica.Models.Usuario();
+
+                //ahora le agregamos el valor obtenido por la fila aL Id
+
+
+                MiUsuarioLocal.UsuarioID = IdUsuario;
+
+                //una vez que tengo el objeto local con el valor del id, puedo ir a consultar 
+                //el usuario por ese id y llenar el resto de atributos
+
+                MiUsuarioLocal = MiUsuarioLocal.ConsultarPorIDRetornaUsuario();
+
+                //validamos que el usuario local tenga datos
+
+                if(MiUsuarioLocal != null && MiUsuarioLocal.UsuarioID > 0)
+                {
+                    //si cargamos correctamente el usuario local llenamos los controles
+
+                    TxtUsuarioID.Text =Convert.ToString(MiUsuarioLocal.UsuarioID);
+
+                    TxtUsuarioNombre.Text = MiUsuarioLocal.UsuarioTelefono;
+
+                    TxtUsuarioCedula.Text = MiUsuarioLocal.UsuarioCedula;
+                    TxtUsuarioTelefono.Text = MiUsuarioLocal.UsuarioTelefono;
+                    TxtUsuarioCorreo.Text = MiUsuarioLocal.UsuarioCorreo;
+                    TxtUsuarioDireccion.Text = MiUsuarioLocal.UsuarioDireccion;
+
+                    //combobox
+                    CbRolesUsuario.SelectedValue = MiUsuarioLocal.MiRolTipo.UsuarioRolID;
+
+
+                    //TODO: Desactivar botones que no son necesarios en este caso el de Agregar 
 
 
 
+                }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        }
     }
 }
